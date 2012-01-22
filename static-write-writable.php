ff0e67f8476433e9ec56a8761947c6a88f8535d2
Joseph Brower <jbrower@jbrower-Bonobo-Professional.(none)>
2012-01-22 12:36:40 -0700

    fixed non-variable thing

--- static-write-writable.php
+++ static-write-writable.php
@@ -2,6 +2,6 @@
   $myFile = "/var/www/benchmark/testWrite.php";
   $fh = fopen($myFile, 'w') or die("can't open file");
   $stringData = "My stuff that is written\n";
-  $fwrite($fh, $stringData);
-  $fclose($fh);
+  fwrite($fh, $stringData);
+  fclose($fh);
 ?>
