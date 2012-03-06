22b867ff73afe4dd638a60ae0f955b3b80d86863
Joseph Brower <jbrower@jbrower-Bonobo-Professional.(none)>
2012-03-05 17:19:19 -0700

    made mysql.php use db2

--- mysql.php
+++ mysql.php
@@ -1,11 +1,11 @@
 <?php
-$con = mysql_connect($_SERVER['DB1_HOST'],$_SERVER['DB1_USER'],$_SERVER['DB1_PASS']);
+$con = mysql_connect($_SERVER['DB2_HOST'],$_SERVER['DB2_USER'],$_SERVER['DB2_PASS']);
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
   
-mysql_select_db($_SERVER['DB1_NAME'], $con);
+mysql_select_db($_SERVER['DB2_NAME'], $con);
 
 $result1 = mysql_query("SELECT value FROM core_config_data WHERE path = 'web/cookie/cookie_lifetime' AND scope = 'default' AND scope_id = 0");
 
