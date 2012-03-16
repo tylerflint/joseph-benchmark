d13091173e56024ea8d79d71c675df887d0ed1b1
Joseph Brower <jbrower@jbrower-Bonobo-Professional.(none)>
2012-03-16 14:22:11 -0600

    good benchmark now

--- benchredis.php
+++ benchredis.php
@@ -1,5 +1,21 @@
 <?php
-$redis->connect('tunnel.pagodabx.com', 6379);
-$redis->set('key','value');
-echo $redis->get('key');
-?>
+define('TEST_KEY', 'are_we_glued');
+$redis = new Redis();
+try {
+    $redis->connect('tunnel.pagodabox.com', 6379);
+    $redis->set(TEST_KEY, 'yes');
+    $glueStatus = $redis->get(TEST_KEY);
+    if ($glueStatus) {
+        $testKey = TEST_KEY;
+        echo "Glued with the Redis key value store:" . PHP_EOL;
+        echo "1. Got value '{$glueStatus}' for key '{$testKey}'." . PHP_EOL;
+        if ($redis->delete(TEST_KEY)) {
+            echo "2. And already removed the key/value pair again." . PHP_EOL;
+        }
+    } else {
+        echo "Not glued with the Redis key value store." . PHP_EOL;
+    }
+} catch (RedisException $e) {
+    $exceptionMessage = $e->getMessage();
+    echo "{$exceptionMessage}. Not glued with the Redis key value store." . PHP_EOL;
+}
